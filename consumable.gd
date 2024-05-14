21d3f76abe56e47ac199ad5a61600cd2cc91a653
loteque <andrewandroid@gmail.com>
2024-05-14 03:37:53 -0700

    demo doesn't crash but signals broken

--- consumable.gd
+++ consumable.gd
@@ -1,15 +1,26 @@
 extends Resource
 class_name Consumable
 
-@export var name: String
+@export var id: String
 @export var amount: int
-@export var regen: bool
 @export var min_amount: int
 @export var max_amount: int
+@export var inventory_id: String
+@export var inventory_index: int
 
+## TODO: need unique signals for each instance of consumable
 signal amount_updated(name, amount)
 
 func update_amount(new_amount: int):
-    amount_updated.emit(name, new_amount)
+    amount_updated.emit(id, new_amount)
     amount = new_amount
+
+func create_consumable():
     
+    var consumable = Consumable.new()
+    consumable.id = id
+    consumable.amount = amount
+    consumable.min_amount = min_amount
+    consumable.max_amount = max_amount
+    consumable.inventory_id = inventory_id
+    consumable.inventory_index = inventory_index
--- inventory.gd
+++ inventory.gd
@@ -1,46 +1,36 @@
-extends Resource
+extends Consumable
 class_name Inventory
 
-@export var inventory_name: String 
 @export var consumables: Array[Consumable]
 
-signal consumable_added(name, amount)
-signal consumable_subtracted(name, amount)
-signal consumable_created(name, amount)
-
-func get_consumable_by_name(consumable_name) -> Consumable:
+func get_consumable_by_name(consumable_id) -> Consumable:
     var selected: Consumable
     if consumables.is_empty():
         print("inventory is empty")
 
     # gonna need a better search
     for consumable in consumables:
-        if consumable.name == consumable_name:
+        if consumable.id == consumable_id:
             selected = consumable
 
     return selected
 
-func add_consumable(consumable: Consumable, amount: int):
+func add_consumable(consumable: Consumable, cons_amount: int, cons_index: int):
     var selected = get_consumable_by_name(consumable.name)
 
     if selected:
-        var new_amount = selected.amount + amount
+        var new_amount = selected.amount + cons_amount
         selected.amount = new_amount
     else:
-        consumables.append(consumable)
-        consumable_created.emit(consumable.name, amount)
+        consumables.insert(cons_index, selected)
 
-    consumable_added.emit(consumable.name, amount)
-
-func subtract_consumable(consumable: Consumable, amount: int):
-    var selected = get_consumable_by_name(consumable.name)
+func remove_consumable(c_id: String):
+    var selected = get_consumable_by_name(c_id)
     if !selected:
         return
 
     if selected:
-        selected.amount = selected.amount - amount
-
-    consumable_subtracted.emit(consumable.name, amount)
+        consumables[selected.inventory_index] = null
 
 
 
