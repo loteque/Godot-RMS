0e8e21aa7fa2077948e77487b6c7efad5e4aba28
loteque <andrewandroid@gmail.com>
2024-05-14 04:53:12 -0700

    removed unessicary function, added inline notes

--- consumable.gd
+++ consumable.gd
@@ -8,23 +8,11 @@ class_name Consumable
 @export var inventory_id: String
 @export var inventory_index: int
 
-## TODO: need unique signals for each instance of consumable
-#signal amount_updated(name, amount)
-
 var signal_name
 
 func update_amount(new_amount: int):
     amount = new_amount
     emit_signal(signal_name, id, amount)
-
-func create_consumable(name):
-    
-    var consumable = Consumable.new(name)
-    consumable.amount = amount
-    consumable.min_amount = min_amount
-    consumable.max_amount = max_amount
-    consumable.inventory_id = inventory_id
-    consumable.inventory_index = inventory_index
 
 func _init(name: String):
     id = name
--- demo/consumsable_panel.gd
+++ demo/consumsable_panel.gd
@@ -3,8 +3,17 @@ extends PanelContainer
 @export var name_label: Label
 @export var amount_label: Label
 @export var consume_button: Button
-@export var consumable: Consumable
 @export var consumer: Consumer
+
+
+# defining this here feels wierd.
+# this is defining a connection
+# maybe connection[sender, reciever]
+# or maybe define in consumer like:
+# consumer.sender
+# consumer.reciever
+
+@export var consumable: Consumable
 @export var reciever: Consumable
 
 func _on_consume_pressed():
