e6bc0b08b81aa92248fe8e0d417e64e89d166706
loteque <andrewandroid@gmail.com>
2024-05-15 21:31:57 -0700

    many changes to reduce complexity and catpure edge cases

--- demo/consumsable_panel.gd
+++ demo/consumsable_panel.gd
@@ -3,28 +3,45 @@ extends PanelContainer
 @export var name_label: Label
 @export var amount_label: Label
 @export var consume_button: Button
-@export var consumer: Consumer
 
+var container: ResourceContainer
 
-# defining this here feels wierd.
-# this is defining a connection
-# maybe connection[sender, reciever]
-# or maybe define in consumer like:
-# consumer.sender
-# consumer.reciever
+func create_container(
+                    con_nam: String, 
+                    con_amt: int, 
+                    con_min_amt: int, 
+                    con_max_amt: int, 
+                    inv_id: String = "", 
+                    inv_idx: int = 0
+                    ):
 
-@export var consumable: Consumable
-@export var reciever: Consumable
+    container = ResourceContainer.new(
+                            con_nam, 
+                            con_amt, 
+                            con_min_amt, 
+                            con_max_amt, 
+                            inv_id, 
+                            inv_idx
+                            )
+
+    name_label.text = con_nam
+    amount_label.text = str(con_amt)
+    name = con_nam
+
+    var sig_str = container.get_signal_str()
+    container.connect(sig_str, _on_updated)
+
+    return self
+
+func _on_updated(transaction_name, amount, remainder):
+    amount_label.text = str(amount)
+    print("caught container signal from: " 
+    + transaction_name + 
+    ", amount: " 
+    + str(amount) + 
+    ", with remainder: " 
+    + str(remainder))
 
 func _on_consume_pressed():
-    consumable.update_amount(
-        consumer.subtract(
-            consumable
-            )
-        )
-    if reciever:
-        reciever.update_amount(
-            consumer.add(
-                reciever
-            )
-        )
+    container.transaction.execute()
+    print("_on_consume_pressed")
---
+++ transaction.gd
@@ -0,0 +1,26 @@
+extends Resource
+class_name Transaction
+
+@export var name: String
+@export var is_auto: bool
+@export var rate: int
+@export var sender: ResourceContainer
+@export var reciever: ResourceContainer
+
+func execute():
+    if sender == reciever:
+        sender.subtract(rate)
+    
+    elif sender != reciever:
+        if sender:
+            sender.subtract(rate)
+    
+        if reciever:
+            reciever.add(rate)
+
+func _init(send: ResourceContainer, recieve: ResourceContainer, nam: String = "", auto: bool = false, r: int = 0):
+    reciever = recieve
+    sender = send
+    name = nam
+    is_auto = auto
+    rate = r
